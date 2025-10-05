90050cc00a642cdcc14a65d3bc431f607ac8e209
rafiqwe <rmlmanik502@gmail.com>
2025-10-05 22:14:03 +0600

    Created Has No more post component

---
+++ app/components/feed/HasNoMorePost.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface IHasNoMore{
+    hasMore: boolean;
+    postLength: number;
+}
+const HasNoMorePost: React.FC<IHasNoMore> = ({hasMore, postLength}) => {
+  return (
+    <div>
+        {!hasMore && postLength > 0 && (
+             <div className="text-center py-8 text-gray-500">
+               <p>{`You've`} reached the end! 🎉</p>
+             </div>
+       )}
+    </div>
+  )
+}
+
+export default HasNoMorePost
