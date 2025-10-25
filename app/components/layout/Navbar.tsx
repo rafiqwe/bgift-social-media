506178f740bebfc8162f5b2e1d551d0f40760660
rafiqwe <rmlmanik502@gmail.com>
2025-10-25 23:10:07 +0600

    Update navbar component

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -2,16 +2,15 @@ import { auth } from "@/lib/auth";
 import NavLogo from "./NavLogo";
 import NavRightSection from "./NavRightSection";
 
-
 export default async function Navbar() {
-    const session = await auth()
+  const session = await auth();
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white border-b border-gray-200 z-50">
+    <nav className="fixed top-12 left-0 right-0 bg-white border-b border-gray-200 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <NavLogo/>
+          <NavLogo />
           {/* Search Bar - Hidden on mobile */}
           <div className="hidden md:block flex-1 max-w-md mx-8">
             <input
@@ -21,7 +20,7 @@ export default async function Navbar() {
             />
           </div>
           {/* Right Section */}
-          <NavRightSection user={session?.user}/>
+          <NavRightSection user={session?.user} />
         </div>
       </div>
     </nav>
