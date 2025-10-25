d0db16ed9da189db4efc496ac487a3c268cb96ab
rafiqwe <rmlmanik502@gmail.com>
2025-10-25 23:12:29 +0600

    Updated comments component

--- app/components/Comment/Comments.tsx
+++ app/components/Comment/Comments.tsx
@@ -8,6 +8,7 @@ import React, { useState, useRef, useEffect } from "react";
 import CommentMenuOpen from "./CommentMenuOpen";
 import CommentEditModal from "./CommentEditModel";
 import CommentDeleteModal from "./CommentDeleteModel";
+import CommentReportModel from "../Reports/CommentReportModel";
 
 interface CommentProps {
   comment: {
@@ -35,7 +36,7 @@ const Comments: React.FC<CommentProps> = ({
   const menuRef = useRef<HTMLDivElement | null>(null);
   const [showEditModal, setShowEditModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-
+  const [isReportOpen, setIsReportOpen] = useState(false);
   // Close menu on outside click
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -48,39 +49,43 @@ const Comments: React.FC<CommentProps> = ({
   }, []);
   // console.log(comment);
 
+  const handleReportSubmit = () => {
+    console.log("hello from report post ");
+  };
+
   return (
     <div
-      key={comment.id}
+      key={comment?.id}
       className="flex items-start justify-between px-2 py-2 hover:bg-gray-50 rounded-lg transition"
     >
       {/* Left: Avatar + Content */}
       <div className="flex">
         <div className="w-9 h-9 rounded-full overflow-hidden bg-gray-200">
-          <Link href={`/profile/${comment.author.id}`}>
-            {comment.author.image ? (
+          <Link href={`/profile/${comment?.author.id}`}>
+            {comment?.author.image ? (
               <Image
-                src={comment.author.image}
-                alt={comment.author.name}
+                src={comment?.author.image}
+                alt={comment?.author.name}
                 width={36}
                 height={36}
                 className="object-cover"
               />
             ) : (
               <div className="w-full h-full flex items-center justify-center bg-blue-500 text-white text-xs font-bold">
-                {comment.author.name.charAt(0)}
+                {comment?.author.name.charAt(0)}
               </div>
             )}
           </Link>
         </div>
         <div className="px-2 text-sm">
-          <Link href={`/profile/${comment.author.id}`}>
-            <p className="font-medium">{comment.author.name}</p>
+          <Link href={`/profile/${comment?.author.id}`}>
+            <p className="font-medium">{comment?.author.name}</p>
             <span className="text-xs relative -top-1 text-gray-400">
-              @{comment.author.username}
+              @{comment?.author.username}
             </span>
           </Link>
           <div className="text-sm mt-1">
-            <p>{comment.content}</p>
+            <p>{comment?.content}</p>
           </div>
         </div>
       </div>
@@ -104,15 +109,16 @@ const Comments: React.FC<CommentProps> = ({
 
           {isMenuOpen && (
             <CommentMenuOpen
+              onCommentReportOpen={() => setIsReportOpen(true)}
               onEditMenu={() => setShowEditModal(true)}
               onDeleteMenu={() => setShowDeleteModal(true)}
-              isOwnComment={comment.isOwnComment}
-              commentId={comment.id}
+              isOwnComment={comment?.isOwnComment}
+              commentId={comment?.id}
             />
           )}
           {showEditModal && (
             <CommentEditModal
-              comment={{ id: comment.id, content: comment.content }}
+              comment={{ id: comment?.id, content: comment?.content }}
               onClose={() => setShowEditModal(false)}
               onUpdate={handleUpdateComment}
             />
@@ -120,11 +126,17 @@ const Comments: React.FC<CommentProps> = ({
 
           {showDeleteModal && (
             <CommentDeleteModal
-              commentId={comment.id}
+              commentId={comment?.id}
               onClose={() => setShowDeleteModal(false)}
               onDelete={handleDeleteComment}
             />
           )}
+
+          <CommentReportModel
+            isOpen={isReportOpen}
+            onClose={() => setIsReportOpen(false)}
+            onSubmit={handleReportSubmit}
+          />
         </div>
       </div>
     </div>
