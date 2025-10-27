ff3d9a288445932c7bc2eae7571e47bb22695854
rafiqwe <rmlmanik502@gmail.com>
2025-10-27 22:52:02 +0600

    Updated report bug component

--- app/components/reportBug/reportBugs.tsx
+++ app/components/reportBug/reportBugs.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const ReportBugs = () => {
   return (
     <div className="bg-[linear-gradient(95deg,rgb(0,112,116)_0%,rgb(36,167,107)_100%)] py-3 flex w-full items-center justify-center fixed top-0 z-100 left-0 right-0">
-      <h1 className="text-lg font-bold text-white">
+      <h1 className="text-md font-bold text-white">
         Report a Bug
         <span className="text-white/70"> (Beta)</span>
         <Link href={"/report-bug"}>
