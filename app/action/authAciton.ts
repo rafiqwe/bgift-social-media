36571f203abf8c7c11e50569cccfb986e797d132
dev_x_coder <rmlmanik502@gmail.com>
2026-08-20 00:59:03 +0600

    Updated auth action

--- app/action/authAciton.ts
+++ app/action/authAciton.ts
@@ -1,10 +1,13 @@
-'use server'
+"use server";
 
 import { signIn } from "@/lib/auth";
 
-export const signInSocialMedia = async (formData: FormData): Promise<void> => {
-    const action = formData.get('action') as string;
-    await signIn(action, { redirectTo: '/feed' });
-}
-
-
+export const signInSocialMedia = async (
+  formData: FormData | string
+): Promise<void> => {
+  const action =
+    typeof formData === "string"
+      ? formData
+      : (formData.get("action") as string);
+  await signIn(action, { redirectTo: "/feed" });
+};
